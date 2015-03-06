a17bedbc8187ed453c92d72a5b424cdd8b189b4f
Devin P <devinfitzsimons@yahoo.com>
2015-03-06 16:58:13 -0500

    started planning statements

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Create Database Statement
+CREATE DATABASE tournament;
 
+-- something like
+CREATE TABLE matches;
+-- CREATE TABLE matches
+	-- has matchID (integer), player1(playerID), player2(playerID), winner(playerID), loser(playerID)
+			-- CAN'T be same playerID also for Winner and loser
+-- will have 2 players per match
+-- will have some rule that describes who should play who i think
+
+-- somthing like
+-- CREATE TABLE players 
+	-- has playerID (integer), wins(int), losses(int), (matches played)
+CREATE TABLE players;
+-- will have data about each player so
+-- number of matches played
+-- won / loss record
+-- player id
